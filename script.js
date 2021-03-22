cacd8bcb537139c10efacb962f4cde50d0293f4e
arne0512 <arn.clerc@gmail.com>
2021-03-22 10:06:19 +0100

    Lundi 22 MAJ Forms

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ closeActivities.addEventListener("clicl", function(){
     article.style.display = "none";
 })
 
+//Début pop up bouton valider réservation//
+const validationButton = document.querySelector(".bouton-booking")
+const bookingDone = "Votre réservation a bien été prise en compte.";
+validationButton.addEventListener('click', function(){
+    alert(bookingDone);
+});
+//Fin pop up bouton valider réservation//
+
+//Début pop up bouton soumettre formulaire//
+const validationForm = document.querySelector(".bouton-form")
+const formSent = "Votre message a bien été envoyé.";
+validationForm.addEventListener('click', function(){
+    alert(formSent);
+});
+//Fin pop up bouton soumettre formulaire//
