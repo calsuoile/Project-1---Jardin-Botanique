01c76180c773058bb3a1e079ccf77bae20d35d7f
arne0512 <arn.clerc@gmail.com>
2021-03-25 19:18:32 +0100

    MAJ jeudi soir Arnaud

--- script.js
+++ script.js
@@ -14,51 +14,93 @@ for (let i = 0; i < closeActivities.length; i++) {
   });
 }
 
-//Début pop up bouton valider réservation//
-const validationButton = document.getElementById("valid-button1");
+//Début pop up bouton valider réservation
+//const validationButton = document.getElementById("valid-button1");
+////const bookingDone = "Votre réservation a bien été prise en compte.";
+//validationButton.addEventListener("click", function () {
+//alert(bookingDone);
+//});
+const bookingValid = document.getElementById("valid-button1");
+const aGe = document.getElementById("age");
+const missAge = document.getElementById("missAge");
+const nomPrenom1 = document.getElementById("nom1");
+const missNomPrenom1 = document.getElementById("missNomPrenom1");
+const numberPerson = document.getElementById("person-number");
+const missPersonNumber = document.getElementById("missPersonNumber");
 const bookingDone = "Votre réservation a bien été prise en compte.";
-validationButton.addEventListener("click", function () {
-  alert(bookingDone);
-});
-//Fin pop up bouton valider réservation//
 
-//Début pop up bouton soumettre formulaire//
-const validationForm = document.getElementById("valid-button2");
+bookingValid.addEventListener("click", validationBooking);
+
+function validationBooking(event) {
+  if (aGe.validity.valueMissing) {
+    event.preventDefault();
+    missAge.innerHTML = "Champs non renseigné";
+    missAge.style.color = "red";
+    aGe.style.border = "1px solid red";
+  }
+  if (nomPrenom1.validity.valueMissing) {
+    event.preventDefault();
+    missNomPrenom1.innerHTML = "Champs non renseigné";
+    missNomPrenom1.style.color = "red";
+    nomPrenom1.style.border = "1px solid red";
+  }
+  if (numberPerson.validity.valueMissing) {
+    event.preventDefault();
+    missPersonNumber.innerHTML = "Champs non renseigné";
+    missPersonNumber.style.color = "red";
+    numberPerson.style.border = "1px solid red";
+  }
+  if (
+    !aGe.validity.valueMissing &&
+    !nomPrenom1.validity.valueMissing &&
+    !numberPerson.validity.valueMissing
+  ) {
+    alert(bookingDone);
+  }
+}
+
+//Fin pop up bouton valider réservation
+
+//Début formulaire contact
+const formValid = document.getElementById("valid-button2");
+const nomPrenom = document.getElementById("nom");
+const missNomPrenom = document.getElementById("missNomPrenom");
+const eMail = document.getElementById("email");
+const missMail = document.getElementById("missMail");
+const mesSage = document.getElementById("message");
+const missMessage = document.getElementById("missMsg");
 const formSent = "Votre message a bien été envoyé.";
-validationForm.addEventListener("click", function () {
-  alert(formSent);
-});
-//Fin pop up bouton soumettre formulaire//
-
-//Début Alerte champs vide formulaire contact//
-//const formValid = document.getElementById("valid-button2");
-//const nomPrenom = document.getElementById("nom");
-//const missNomPrenom = document.getElementById("missNomPrenom");
-//const formSent = "Votre message a bien été envoyé.";
-
-//formValid.addEventListener('click', validation);
-
-//function validation(event){
-//if (nomPrenom.validity.valueMissing){
-//event.preventDefault();
-//missNomPrenom.innerHTML = "Champs non renseigné";
-//missNomPrenom.style.color = "red";
-//}else {
-//alert(formSent)
-//}
-//}
 
 formValid.addEventListener("click", validation);
 
 function validation(event) {
   if (nomPrenom.validity.valueMissing) {
     event.preventDefault();
-    missNomPrenom.textContent = "Champs non renseigné";
+    missNomPrenom.innerHTML = "Champs non renseigné";
     missNomPrenom.style.color = "red";
-  } else {
+    nomPrenom.style.border = "1px solid red";
+  }
+  if (eMail.validity.valueMissing) {
+    event.preventDefault();
+    missMail.innerHTML = "Champs non renseigné";
+    missMail.style.color = "red";
+    eMail.style.border = "1px solid red";
+  }
+  if (mesSage.validity.valueMissing) {
+    event.preventDefault();
+    missMessage.innerHTML = "Champs non renseigné";
+    missMessage.style.color = "red";
+    mesSage.style.border = "1px solid red";
+  }
+  if (
+    !nomPrenom.validity.valueMissing &&
+    !eMail.validity.valueMissing &&
+    !mesSage.validity.valueMissing
+  ) {
     alert(formSent);
   }
 }
+//Fin formulaire contact
 
 /*menu burger*/
 const burgerMenu = document.getElementById("burger");
