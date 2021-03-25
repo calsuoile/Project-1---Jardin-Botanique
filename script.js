6a98a63de94a464644e4cf4eae52060c9be15edb
NicolasGeneste <nicolasgeneste@gmail.com>
2021-03-25 09:58:44 +0100

    Maj Nico Jeudi

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-
 const openActivities = document.querySelectorAll(".openActivities");
 const article = document.querySelectorAll(".article");
 const closeActivities = document.querySelectorAll(".closeActivities");
 
-for (let i = 0; i < openActivities.length; i ++){
-    openActivities[i].addEventListener("click", function(){
-        article[i].style.display = "block";
-    }
-)}
+for (let i = 0; i < openActivities.length; i++) {
+  openActivities[i].addEventListener("click", function () {
+    article[i].style.display = "block";
+  });
+}
 
-for (let i = 0; i < closeActivities.length; i ++){
-    closeActivities[i].addEventListener("click", function(){
-        article[i].style.display = "none";
-    }
-)}
+for (let i = 0; i < closeActivities.length; i++) {
+  closeActivities[i].addEventListener("click", function () {
+    article[i].style.display = "none";
+  });
+}
 
 //Début pop up bouton valider réservation//
 const validationButton = document.getElementById("valid-button1");
 const bookingDone = "Votre réservation a bien été prise en compte.";
-validationButton.addEventListener('click', function(){
-    alert(bookingDone);
+validationButton.addEventListener("click", function () {
+  alert(bookingDone);
 });
 //Fin pop up bouton valider réservation//
 
 //Début pop up bouton soumettre formulaire//
 const validationForm = document.getElementById("valid-button2");
 const formSent = "Votre message a bien été envoyé.";
-validationForm.addEventListener('click', function(){
-    alert(formSent);
+validationForm.addEventListener("click", function () {
+  alert(formSent);
 });
 //Fin pop up bouton soumettre formulaire//
 
@@ -36,23 +35,19 @@ const formValid = document.getElementById("valid-button2");
 const nomPrenom = document.getElementById("nom");
 const missNomPrenom = document.getElementById("missNomPrenom");
 
+/*<div id="menuburger">
+  <div class="bar1"></div>
+  <div class="bar2"></div>
+  <div class="bar3"></div>
+</div>;*/
+formValid.addEventListener("click", validation);
 
-
-
-
-<div id="menuburger">
-<div class="bar1"></div>
-<div class="bar2"></div>
-<div class="bar3"></div>
-</div>
-formValid.addEventListener('click', validation);
-
-function validation(event){
-    if (nomPrenom.validity.valueMissing){
-        event.preventDefault();
-        missNomPrenom.textContent = "Champs non renseigné";
-        missNomPrenom.style.color = "red";
-    }else {
-        alert(formSent);
-    }
+function validation(event) {
+  if (nomPrenom.validity.valueMissing) {
+    event.preventDefault();
+    missNomPrenom.textContent = "Champs non renseigné";
+    missNomPrenom.style.color = "red";
+  } else {
+    alert(formSent);
+  }
 }
